c7320e6f9b0a8d7f2f8b45ca6a5a6a4fee48d6de
mukti222 <135799527+mukti222@users.noreply.github.com>
2024-03-23 22:36:13 +0700

    Delete backend directory

--- backend/generate.js
+++
@@ -1,7 +0,0 @@
-const jwt = require ('jsonwebtoken');
-
-const generateAccessToken = (email) => {
-  return jwt.sign({ email }, 'AOKWAWK', { expiresIn: '1h' });
-}
-
-module.exports = { generateAccessToken }
